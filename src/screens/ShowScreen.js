f9bd0b7002d2269be6520339de007bd3c247e065
raghav-sharma272381 <72136954+raghav-sharma272381@users.noreply.github.com>
2021-04-03 09:12:36 +0530

    Add files via upload

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons';
+
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
+  );
+
+  return (
+    <View>
+      <Text style={styles.title}>{blogPost.title}</Text>
+      <Text style={styles.Text}>{blogPost.content}</Text>
+    </View>
+  );
+};
+
+ShowScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Edit', { id: navigation.getParam('id') })
+        }
+      >
+        <EvilIcons name="pencil" size={35} />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  title:{
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontSize: 30,
+    marginBottom:30
+  },
+  Text:{
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontSize:15
+  }
+});
+
+export default ShowScreen;
